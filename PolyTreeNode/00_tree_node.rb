94598e52e975c569bb63926662293ffa38fac7ce
Stephen Clark <stephenmclark527@gmail.com>
2019-10-20 18:48:41 -0400

    Wrote and tested PolyTreeNode class

--- PolyTreeNode/00_tree_node.rb
+++ PolyTreeNode/00_tree_node.rb
@@ -18,4 +18,35 @@ class PolyTreeNode
     end
   end
 
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    if child_node.parent == nil
+      raise "This node is not a child."
+    else
+      child_node.parent = nil
+    end
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    children.each do |child|
+      target_node = child.dfs(target_value)
+      return target_node unless target_node.nil?
+    end
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue += current_node.children
+    end
+    nil
+  end
 end
